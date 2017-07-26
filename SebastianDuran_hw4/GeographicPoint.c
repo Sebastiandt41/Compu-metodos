86235563dcf33de89f049f698c737b33017c8313
Sebastiandt41 <sebastian4131@gmail.com>
2017-07-25 22:16:01 -0500

    Posiciones OK

--- SebastianDuran_hw4/GeographicPoint.c
+++ SebastianDuran_hw4/GeographicPoint.c
@@ -2,4 +2,57 @@
 #include <math.h>
 #include <stdio.h>
 
+void cargar_datos(char Archiv);
+int indice(int x , int y);
+float get_radio(int x0, int x1, int y0, int y1);
+int sampleo_mc();
+
+	
+	int largo = 744;
+	int alto = 500;
+	int i ;
+	int j ;
+
+int main(void)
+{
+	int *posicion = malloc((largo*alto)*sizeof(int));
+
+	FILE *in;
+	char mapa[20] = "map_data.txt";
+	in = fopen(mapa,"r");
+	for(j=0;j<alto;j++)
+	{
+		for(i=0;i<largo;i++)
+		{
+			fscanf(in,"%d\n",&posicion[indice(i,j)]);	
+		}
+	}
+	printf("posicion = %d\n",posicion[indice(217,236)]); //=0
+	printf("posicion = %d\n",posicion[indice(230,234)]); //=1
+
+
+return 0;
+}
+
+
+
+int indice(int x, int y)
+{
+	int ind = alto*x+y;
+	return ind;
+}
+
+float get_radio(int x0,int x1,int y0,int y1)
+{
+	float r = sqrt(((x1-x0)*(x1-x0))+((y1-y0)*(y1-y0)));
+	return r;
+}
+
+
+
+
+
+
+
+
 
--- SebastianDuran_hw4/Plots.py
+++ SebastianDuran_hw4/Plots.py
@@ -1,11 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt 
 
-datos = np.genfromtxt("map_data.txt")
+datos = np.genfromtxt("map_data.txt",delimiter=" ")
 
 
 plt.imshow(datos)
-plt.plot(74,05)
+#plt.plot(74,05)
 #plt.plot(x,y)
 plt.show()
 plt.close()
