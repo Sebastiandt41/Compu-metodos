2e5eed5699b586545b202e57c947068d2f5c69a2
Sebastiandt41 <you@example.com>
2017-07-26 19:12:08 -0500

    2.0

--- SebastianDuran_hw4/Plots.py
+++ SebastianDuran_hw4/Plots.py
@@ -5,8 +5,10 @@ datos = np.genfromtxt("map_data.txt",delimiter=" ")
 
 print datos[236,217],datos[234,230],datos[62,702],datos[62,703]
 
-#plt.imshow(datos)
-#plt.plot(74,05)
+
+plt.imshow(datos)
+#circle1  = plt.Circle((327, 123), 100, color='b', fill=False)
+plt.plot(123,327,"o")
 #plt.plot(x,y)
 plt.show()
 plt.close()
