164954e21f211929a64fa694012c3cd8264aef9f
Sebastiandt41 <you@example.com>
2017-07-26 19:02:55 -0500

    func

--- SebastianDuran_hw4/GeographicPoint.c
+++ SebastianDuran_hw4/GeographicPoint.c
@@ -1,12 +1,10 @@
 #include <stdlib.h>
 #include <math.h>
 #include <stdio.h>
+#include <time.h>
 
-void cargar_datos(char Archiv);
 int indice(int x , int y);
-//float get_radio(int x0, int x1, int y0, int y1);
 float radius(int posx,int posy, int *posicion);
-int sampleo_mc(void);
 int aleatorio_x();
 int aleatorio_y();
 
@@ -17,6 +15,7 @@ int j ;
 
 int main(void)
 {
+	//Cargo los datos 
 	int *posicion = malloc((largo*alto)*sizeof(int));
 	FILE *in;
 	char mapa[20] = "map_data.txt";
@@ -29,16 +28,69 @@ int main(void)
 		//printf("posicion %d\n",posicion[indice(i,j)]);
 		}
 	}
+	fclose(in);
+	
 
-	int xa = aleatorio_x();
-	int ya = aleatorio_y();
-	float ra = radius(480,136,posicion);
-	printf("ra = %f\n",ra);
+	srand(time(NULL));
+	
+	
+	//printf("ra = %f\n",ra);
 	
 	//printf("aleatorios = %d %d\n",x_a,y_a);
 
 	//printf("posicion = %d\n",posicion[indice(217,236)]); //=0
 	//printf("posicion = %d\n",posicion[indice(230,234)]); //=1
+
+	//MARKOV CHAIN 
+	int n_pasos = 100;
+	int xa = aleatorio_x();
+	int ya = aleatorio_y();
+	float r_inicial = radius(xa,ya,posicion);
+	float r_new;
+	int x_new, y_new;
+	float alpha;
+
+	for(i=0; i < n_pasos;i++)
+	{
+		x_new = xa+aleatorio_x();
+		if(x_new<0)
+		{x_new = largo + x_new%largo;}
+		else if(x_new >largo)
+		{x_new =x_new%largo;}
+		y_new = ya+aleatorio_y();
+		if(y_new<0)
+		{y_new = alto + y_new%alto;}
+		else if(y_new>alto)
+		{y_new=y_new%largo;}	
+	
+	r_new = radius(x_new,y_new,posicion);
+	
+	alpha = r_new/r_inicial ;
+	
+		if(alpha > 1)
+		{
+		xa = x_new;
+		ya = y_new;
+		r_inicial = r_new;
+		}
+		else if(alpha <1)
+		{
+		float beta = rand()/RAND_MAX;
+			if(beta < alpha)
+			{
+			xa = x_new;
+			ya = y_new;
+			r_inicial = r_new;
+			}
+			else if(beta > alpha)
+			{
+			continue;
+			}
+		}
+
+	}
+	printf("El radio,posx,posy es = %f %d %d\n",r_inicial,xa,ya);
+
 return 0;
 }
 
@@ -60,35 +112,22 @@ return ind;
 
 int aleatorio_x()
 {
-	int x_alea = rand()% largo/5 ;
+	int x_alea = rand()% (largo/5+1) - largo/10 ;
+	//int x_alea = rand()%largo/5;
 	return x_alea;
 }
 int aleatorio_y()
 {
-	int y_alea = rand()% alto/5 ;
+	int y_alea = rand()% (alto/3+1) - largo/6;
+	//int y_alea = rand()%alto/3;
 	return y_alea;
 }
-float radius(int posx,int posy, int *posicion)
+float radius(int posix,int posiy, int *posicion)
 {
-	int posix; 
-	int posiy;
-	if(posicion[indice(posx,posy)]== 1)
-	{
-	posix = aleatorio_x();
-	posiy = aleatorio_y();
-	printf("se fue a la mierda");
-	}
-	else
-	{
-	posix = posx;
-	posiy = posy;
-	printf("fueron los mismos");
-	}
-	
 	float comp;
 	float r_max;
 	float r ;
-	for(r = 1; r < alto/4 ; r++)
+	for(r = 1; r < alto/2 ; r++)
 	{
 		for(i=0;i<r;i++)
 		{
@@ -96,13 +135,11 @@ float radius(int posx,int posy, int *posicion)
 			{
 				if(((i*i)+(j*j))<r*r)
 				{
-					//if(posicion[indice(posix+i,posiy+j)]==0 && posicion[indice(posix-i,posiy-j)]==0 && posicion[indice(posix+i,posiy-j)]==0 && posicion[indice(posix-i,posiy+j)]==0)
 					if(posicion[indice(posix+i,posiy)]==0 && posicion[indice(posix,posiy+j)]==0 && posicion[indice(posix-i,posiy)]==0 && posicion[indice(posix,posiy-j)]==0 && posicion[indice(posix+i,posiy+j)]==0 && posicion[indice(posix-i,posiy-j)]==0 && posicion[indice(posix+i,posiy-j)]==0 && posicion[indice(posix-i,posiy+j)]==0)
 					{
-						//if(posicion[indice(posix+i,posiy+j)]==0 && posicion[indice(posix-i,posiy-j)]==0 && posicion[indice(posix+i,posiy-j)]==0 && posicion[indice(posix-i,posiy+j)]==0)			{
-					r_max = r;
-					//	}
 					
+					r_max = r;
+										
 					}
 					else
 					{
