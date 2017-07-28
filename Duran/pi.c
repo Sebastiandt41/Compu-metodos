c6e90a1d387de1c6d80d4cd0958402ebd614e650
Sebastiandt41 <s.caro11@uniandes.edu.co>
2017-07-28 14:50:19 -0500

    Ejercio 5 laboratorio

---
+++ Duran/pi.c
@@ -0,0 +1,59 @@
+#include <stdlib.h>
+#include <math.h>
+#include <stdio.h>
+#include <time.h>
+
+
+float func(float x);
+float aleat_x();
+float integral(float suma,float dt);
+
+
+int main(void)
+{
+	int i;
+	srand48(time(NULL));
+	int a = 0;
+	int b = 1;	
+	int puntos = 1000000;
+	float dt = ((b-a)/(puntos-1.0));
+	float suma = 0;
+
+	for(i=0;i<puntos;i++)
+	{
+		float valor = func(aleat_x());
+		suma += valor;
+		
+	}
+
+	float resultado = integral(suma,dt);
+
+	FILE *on;
+	char filen[100] = "resultados.txt";
+	on = fopen(filen,"a");
+	fprintf(on,"El valor de la constante pi es :%f\n",resultado);
+	fclose(on);
+	
+return 0;
+}
+
+float func(float x)
+{
+	float y = 4.0/(1+(x*x));
+	return y;
+
+}
+float aleat_x()
+{
+	int max = 1;
+	int min = 0;
+	float x = ((max-min)*drand48() + min);
+	return x;
+}
+
+float integral(float suma,float dt)
+{
+	float integrali = suma*dt;
+	return integrali;
+
+}
